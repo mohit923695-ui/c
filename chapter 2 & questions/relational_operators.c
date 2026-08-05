2c61265c3c8a63214182caf3abafee995ead1511
mohit923695-ui <mohit923695@gmail.com>
2026-08-05 23:44:42 +0530

    program

---
+++ chapter 2 & questions/relational_operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("%d\n",1==2);
+    printf("%d\n",1==1);
+    printf("%d\n",2>=3);
+    printf("%d\n",4<=3);
+    printf("%d\n",3!=6);
+    return 0 ;
+
+}
