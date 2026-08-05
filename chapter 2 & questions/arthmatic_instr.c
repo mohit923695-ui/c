c54c8210fcf8529f462d1b4309e1b49e20a7c1b1
mohit923695-ui <mohit923695@gmail.com>
2026-08-05 23:20:20 +0530

    arthmatic

--- chapter 2 & questions/arthmatic_instr.c
+++ chapter 2 & questions/arthmatic_instr.c
@@ -3,7 +3,18 @@
 #include<stdio.h>
 int main(){
     int a =10;
-    
+    printf("%d",11%2);
     return 0 ;
 
 }
+/*valid instructions
+a = b+c
+a = b*c
+a = b/c*/
+
+/*
+invalid instructions 
+b+c = a
+a = bc
+a = b^c
+*/
